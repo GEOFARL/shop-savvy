64e2b2c4e71dcddba4185fdc7038f8de709dd51c
Maxim <toper.one111@gmail.com>
2023-06-18 19:00:01 +0300

    Add a dummy data sharing

--- routes/shop.js
+++ routes/shop.js
@@ -3,10 +3,12 @@ const path = require('path');
 const express = require('express');
 
 const rootDir = require('../util/path');
+const adminData = require('./admin');
 
 const shopRouter = express.Router();
 
 shopRouter.get('/', (req, res, next) => {
+  console.log(adminData.products);
   res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
