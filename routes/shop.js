f0fcdc6bf8c24ac1db2d595c3beda1fca31ac318
Maxim <toper.one111@gmail.com>
2023-06-18 19:18:17 +0300

    Adding pug templating engine and generating shop page

--- routes/shop.js
+++ routes/shop.js
@@ -8,8 +8,7 @@ const adminData = require('./admin');
 const shopRouter = express.Router();
 
 shopRouter.get('/', (req, res, next) => {
-  console.log(adminData.products);
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
 });
 
 module.exports = shopRouter;
