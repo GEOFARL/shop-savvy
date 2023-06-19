c3e97f97375f38e0cc9739a40a127aa469817075
Maxim <toper.one111@gmail.com>
2023-06-19 09:36:17 +0300

    Adding ejs templates

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
 const path = require('path');
 
 const express = require('express');
-const expressHbs = require('express-handlebars').engine;
+// const expressHbs = require('express-handlebars').engine;
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine(
-  'hbs',
-  expressHbs({
-    defaultLayout: 'main-layout',
-    layoutsDir: 'views/layouts/',
-    extname: 'hbs',
-  })
-);
-app.set('view engine', 'hbs');
+// app.engine(
+//   'hbs',
+//   expressHbs({
+//     defaultLayout: 'main-layout',
+//     layoutsDir: 'views/layouts/',
+//     extname: 'hbs',
+//   })
+// );
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(express.urlencoded({ extended: 'true' }));
