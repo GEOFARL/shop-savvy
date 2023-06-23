047ac6cbd94481bef3ce99a0e2dc932a263bf3dd
Maxim <toper.one111@gmail.com>
2023-06-23 21:26:44 +0300

    Add starting point for rendering an editing page

--- routes/admin.js
+++ routes/admin.js
@@ -6,6 +6,7 @@ const {
   getAddProduct,
   postAddProduct,
   getProducts,
+  getEditProduct,
 } = require('../controllers/admin');
 
 const router = express.Router();
@@ -14,5 +15,7 @@ router.get('/add-product', getAddProduct);
 router.get('/products', getProducts);
 
 router.post('/add-product', postAddProduct);
+
+router.get('/edit-product/:productId', getEditProduct);
 
 module.exports = router;
