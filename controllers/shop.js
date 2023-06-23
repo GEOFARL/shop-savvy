a88c7bc9d9422bb678f847a693a64645b414925d
Maxim <toper.one111@gmail.com>
2023-06-23 21:11:30 +0300

    Adding a cart model

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,3 +1,4 @@
+const Cart = require('../models/cart');
 const Product = require('../models/product');
 
 exports.getProducts = (req, res, next) => {
@@ -40,6 +41,9 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const prodId = req.body.productId;
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
   res.redirect('/cart');
 };
 
