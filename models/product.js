fa44beb1840e334cc5cb228e30536943332d894d
Maxim <toper.one111@gmail.com>
2023-06-23 14:13:30 +0300

    Add a little provisional storage

--- models/product.js
+++ models/product.js
@@ -7,10 +7,38 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(path.dirname(process.mainModule.filename));
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(
+      path.dirname(process.mainModule.filename),
+      'data',
+      'products.json'
+    );
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+
+      cb(JSON.parse(fileContent));
+    });
   }
 };
