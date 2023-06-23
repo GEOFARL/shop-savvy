aad954d9d5ff232910e7dcdb6087aee1fc308510
Maxim <toper.one111@gmail.com>
2023-06-23 19:41:10 +0300

    Create product details page

--- routes/shop.js
+++ routes/shop.js
@@ -8,12 +8,14 @@ const {
   getCart,
   getCheckout,
   getOrders,
+  getProduct,
 } = require('../controllers/shop');
 
 const shopRouter = express.Router();
 
 shopRouter.get('/', getIndex);
 shopRouter.get('/products', getProducts);
+shopRouter.get('/products/:productId', getProduct);
 shopRouter.get('/cart', getCart);
 shopRouter.get('/orders', getOrders);
 shopRouter.get('/checkout', getCheckout);
