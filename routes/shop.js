f2cb4247946f46b8110353394df2965676ae2633
Maxim <toper.one111@gmail.com>
2023-06-23 18:47:09 +0300

    Add orders page

--- routes/shop.js
+++ routes/shop.js
@@ -7,6 +7,7 @@ const {
   getProducts,
   getCart,
   getCheckout,
+  getOrders,
 } = require('../controllers/shop');
 
 const shopRouter = express.Router();
@@ -14,6 +15,7 @@ const shopRouter = express.Router();
 shopRouter.get('/', getIndex);
 shopRouter.get('/products', getProducts);
 shopRouter.get('/cart', getCart);
+shopRouter.get('/orders', getOrders);
 shopRouter.get('/checkout', getCheckout);
 
 module.exports = shopRouter;
