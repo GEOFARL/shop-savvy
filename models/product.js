0264715df8739ecfa533ff3973139d992362abdc
Maxim <toper.one111@gmail.com>
2023-06-26 20:34:33 +0300

    Define Product model with sequelize

--- models/product.js
+++ models/product.js
@@ -1,88 +1,116 @@
-// const fs = require('fs');
-// const path = require('path');
-// const Cart = require('./cart');
+// // const fs = require('fs');
+// // const path = require('path');
+// // const Cart = require('./cart');
 
-const db = require('../util/database');
+// const db = require('../util/database');
 
-// const p = path.join(
-//   path.dirname(process.mainModule.filename),
-//   'data',
-//   'products.json'
-// );
+// // const p = path.join(
+// //   path.dirname(process.mainModule.filename),
+// //   'data',
+// //   'products.json'
+// // );
 
-// const getProductsFromFile = (cb) => {
-//   fs.readFile(p, (err, fileContent) => {
-//     if (err) {
-//       return cb([]);
-//     }
-//     cb(JSON.parse(fileContent));
-//   });
+// // const getProductsFromFile = (cb) => {
+// //   fs.readFile(p, (err, fileContent) => {
+// //     if (err) {
+// //       return cb([]);
+// //     }
+// //     cb(JSON.parse(fileContent));
+// //   });
+// // };
+
+// module.exports = class Product {
+//   constructor(title, imageUrl, price, description, id = null) {
+//     this.id = id;
+//     this.title = title;
+//     this.imageUrl = imageUrl;
+//     this.price = price;
+//     this.description = description;
+//   }
+
+//   // save() {
+//   //   getProductsFromFile((products) => {
+//   //     if (!this.id) {
+//   //       this.id = Math.random().toString();
+//   //       products.push(this);
+//   //       fs.writeFile(p, JSON.stringify(products), (err) => {
+//   //         console.log(err);
+//   //       });
+//   //     } else {
+//   //       const existingProductIndex = products.findIndex(
+//   //         (prod) => prod.id === this.id
+//   //       );
+//   //       const updatedProducts = [...products];
+//   //       updatedProducts[existingProductIndex] = this;
+//   //       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+//   //         console.log(err);
+//   //       });
+//   //     }
+//   //   });
+//   // }
+
+//   save() {
+//     return db.execute(
+//       'INSERT INTO Products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
+//       [this.title, this.price, this.imageUrl, this.description]
+//     );
+//   }
+
+//   // static fetchAll(cb) {
+//   //   getProductsFromFile(cb);
+//   // }
+
+//   static fetchAll() {
+//     return db.execute('SELECT * FROM Products');
+//   }
+
+//   // static findById(id, cb) {
+//   //   getProductsFromFile((products) => {
+//   //     const product = products.find((p) => p.id === id);
+//   //     cb(product);
+//   //   });
+//   // }
+
+//   static findById(id) {
+//     return db.execute('SELECT * FROM Products WHERE products.id = ?', [id]);
+//   }
+//   // static deleteById(id) {
+//   //   getProductsFromFile((products) => {
+//   //     const product = products.find((prod) => prod.id === id);
+//   //     const updatedProducts = products.filter((prod) => prod.id !== id);
+//   //     fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
+//   //       if (!err) {
+//   //         Cart.deleteProduct(id, product.price);
+//   //       }
+//   //     });
+//   //   });
+//   // }
 // };
 
-module.exports = class Product {
-  constructor(title, imageUrl, price, description, id = null) {
-    this.id = id;
-    this.title = title;
-    this.imageUrl = imageUrl;
-    this.price = price;
-    this.description = description;
-  }
+const Sequelize = require('sequelize');
 
-  // save() {
-  //   getProductsFromFile((products) => {
-  //     if (!this.id) {
-  //       this.id = Math.random().toString();
-  //       products.push(this);
-  //       fs.writeFile(p, JSON.stringify(products), (err) => {
-  //         console.log(err);
-  //       });
-  //     } else {
-  //       const existingProductIndex = products.findIndex(
-  //         (prod) => prod.id === this.id
-  //       );
-  //       const updatedProducts = [...products];
-  //       updatedProducts[existingProductIndex] = this;
-  //       fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-  //         console.log(err);
-  //       });
-  //     }
-  //   });
-  // }
+const sequelize = require('../util/database');
 
-  save() {
-    return db.execute(
-      'INSERT INTO Products (title, price, imageUrl, description) VALUES (?, ?, ?, ?)',
-      [this.title, this.price, this.imageUrl, this.description]
-    );
-  }
+const Product = sequelize.define('product', {
+  id: {
+    type: Sequelize.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  title: Sequelize.STRING,
+  price: {
+    type: Sequelize.DOUBLE,
+    allowNull: false,
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+});
 
-  // static fetchAll(cb) {
-  //   getProductsFromFile(cb);
-  // }
-
-  static fetchAll() {
-    return db.execute('SELECT * FROM Products');
-  }
-
-  // static findById(id, cb) {
-  //   getProductsFromFile((products) => {
-  //     const product = products.find((p) => p.id === id);
-  //     cb(product);
-  //   });
-  // }
-
-  static findById(id) {
-    return db.execute('SELECT * FROM Products WHERE products.id = ?', [id]);
-  }
-  // static deleteById(id) {
-  //   getProductsFromFile((products) => {
-  //     const product = products.find((prod) => prod.id === id);
-  //     const updatedProducts = products.filter((prod) => prod.id !== id);
-  //     fs.writeFile(p, JSON.stringify(updatedProducts), (err) => {
-  //       if (!err) {
-  //         Cart.deleteProduct(id, product.price);
-  //       }
-  //     });
-  //   });
-  // }
-};
+module.exports = Product;
--- util/database.js
+++ util/database.js
@@ -1,10 +1,24 @@
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-});
+// const pool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   database: process.env.DB_NAME,
+//   password: process.env.DB_PASSWORD,
+// });
 
-module.exports = pool.promise();
+// module.exports = pool.promise();
+
+const Sequelize = require('sequelize');
+
+const sequelize = new Sequelize(
+  'node-complete',
+  'root',
+  process.env.DB_PASSWORD,
+  {
+    dialect: 'mysql',
+    host: 'localhost',
+  }
+);
+
+module.exports = sequelize;
