b1354b7ab38d63377e7d378cc46c87fdd830ab89
Maxim <toper.one111@gmail.com>
2023-06-27 11:37:54 +0300

    Define a table for a Cart and CartItems

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const express = require('express');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 // const expressHbs = require('express-handlebars').engine;
 
@@ -45,10 +47,14 @@ app.use('/', get404);
 
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
 
 sequelize
-  // .sync({ force: true })
-  .sync()
+  .sync({ force: true })
+  // .sync()
   .then((result) => {
     return User.findByPk(1);
   })
