07424530b450d7aee86b38c709f7d70702df7ffe
Maxim <toper.one111@gmail.com>
2023-07-18 09:47:53 +0300

    Create Login form

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const mongoose = require('mongoose');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 const { get404 } = require('./controllers/error');
 const User = require('./models/user');
 
@@ -48,6 +49,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+app.use(authRoutes);
 
 app.use('/', get404);
 
